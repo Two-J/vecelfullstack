f7db8e90088b0c89282b9d04045d839481847848
Two J <TwoJ@gmail.com>
2023-02-19 18:28:26 +0630

    formidable

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,15 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import { v4 as uuidv4 } from "uuid";
+import fs from "fs";
+import formidable from "formidable";
 const app = express();
 const port = 3000;
 
 dotenv.config();
 const apiUrl = process.env.API_URL;
-
-console.log(apiUrl);
+// console.log(apiUrl);
 app.use(express.static("public"));
 
 const html = `
@@ -22,6 +25,8 @@ const html = `
   </body>
 </html>
 `;
+
+app.use(bodyParser.json());
 app.get("/", (req: Request, res: Response) => {
   res.send("hello");
 });
@@ -30,6 +35,24 @@ app.get("/api", (req: Request, res: Response) => {
 });
 app.get("/api/users", (req: Request, res: Response) => {
   res.send({ name: "nyeinminhtet", email: "nyeinmg@gmailcom", age: 19 });
+});
+
+app.post("/api/upload", (req: Request, res: Response) => {
+  const name = uuidv4();
+  // const fileType = req.header["content-type"].split("/")[1];
+
+  // const writeStream = fs.createWriteStream(`${name}.${fileType}`);
+  // req.pipe(writeStream);
+  const form = formidable({ multiples: true });
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      res.writeHead(err.httpCode || 400, { "Content-Type": "text/plain" });
+      res.end(String(err));
+      return;
+    }
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ fields, files }, null, 2));
+  });
 });
 
 app.listen(port, () => {
--- public/script.js
+++ public/script.js
@@ -1,13 +1,30 @@
+const api = localStorage.getItem("apiUrl");
+
 const fetchData = async () => {
-  const api = localStorage.getItem("apiUrl");
   console.log(api);
   if (api) {
     const response = await fetch(`${api}/users`);
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
   } else {
     window.location.href = "/api";
   }
 };
 
+const uploadFile = async () => {
+  const input = document.querySelector("#fileupload");
+  // console.log(input.files);
+  const formdata = new FormData();
+  const files = [...input.files];
+  files.forEach((file) => {
+    formdata.append("files", file);
+  });
+  console.log(files);
+  const response = await fetch(`${api}/upload`, {
+    method: "POST",
+    body: formdata,
+  });
+  const data = await response.json();
+  console.log(data);
+};
 fetchData();
